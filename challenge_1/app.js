086417e34b73c1061f8c0ecc6ec89f8d08170677
rebz135 <rebz135@gmail.com>
2018-08-27 14:44:03 -0700

    added win checker feature

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -20,15 +20,19 @@ var clickFunc = (event) => {
 		event.target.setAttribute("class", "tile cross");
 		event.target.style = "flex-basis: 30%; background-image: url(images/cross.png);";
 		board[event.target.id[0]][event.target.id[1]] = 'X';
-		console.log('board', board);
+		if (winCheck(board) === 'X') {
+			alert ("CROSS WON!");
+		}
 		counter++;
 
 	} else {
 		event.target.setAttribute("class", "tile circle");
 		event.target.style = "flex-basis: 30%; background-image: url(images/circle.png);";
 		board[event.target.id[0]][event.target.id[1]] = 'O';
-		console.log('board', board);
 		counter++;
+		if (winCheck(board) === 'O') {
+			alert ("CIRCLE WON!");
+		}
 	}
 };
 
@@ -36,10 +40,9 @@ var tilesArr = document.getElementsByClassName("tile");
 console.log('tiles', tilesArr);
 for (var ele of tilesArr) {
 	ele.addEventListener('click', clickFunc, {once: true})
-	console.log('listener added!')
 }
 	
-//CHECK WIN OR TIE
+//BOARD COMPONENT + WIN CHECKER
 var board = [
 	[null, null, null],
 	[null, null, null],
@@ -48,23 +51,27 @@ var board = [
 
 var winCheck = function(board) {
 	let result = null;
-	if (result !== undefined) {
-		return result;
-	}
+	console.log('board', board);
 	for (let row of board) {
-		if (row[0] !== null && row[0] === row[1] === row[2]) {
-			return result = row[0];
+		if (row[0] !== null && row[0] === row[1] && row[1] === row[2]) {
+			result = row[0]
+			return result;
 		};
 	};
 	for (let c=0; c<3; c++) {
-		if (board[0][c] !== null && board[0][c] === board[1][c] === board[2][c]) {
-			return result = board[0][c];
+		if (board[0][c] !== null && board[0][c] === board[1][c] && board[1][c] === board[2][c]) {
+			result = board[0][c];
+			return result;
 		};
 	};
-	if (board[0][0] !== null && board[0][0] === board[1][1] === board[2][2]) {
-		return result = board[0][0];
+	if (board[0][0] !== null && board[0][0] === board[1][1] && board[1][1] === board[2][2]) {
+		result = board[0][0];
+		return result;
 	};
-	if (board[0][2] !== null && board[0][2] === board[1][1] === board[2][0]) {
-		return result = board[0][2];
+	if (board[0][2] !== null && board[0][2] === board[1][1] && board[1][1] === board[2][0]) {
+		result = board[0][2];
+		return result;
 	};
+	console.log('result', result);
+	return result;
 }
